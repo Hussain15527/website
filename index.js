dc9737dde159eaa1838310ee653e3bd91e3e4997
hmodassir <89156644+hmodassir@users.noreply.github.com>
2021-12-30 19:54:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-import './style.css';
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+function increment() {
+  count = count +1
+  countEl.innerText = count
+}
+function save() {
+  let countStr =" " +count + " - "
+  saveEl.textContent += countStr
+}
+
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let name = "modassir"
+// let greeting= "welcome back"
+
+// welcomeEl.innerText = name+greeting
+// welcomeEl.innerText = welcomeEl.innerText + "😈"
